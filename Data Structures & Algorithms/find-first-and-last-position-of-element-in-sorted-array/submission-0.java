f18237c74672a8af8bf7bc459b1d3d45babbe001
Suresh <venkatasuresh051@yahoo.com>
2026-06-29 17:41:15 +0530

    Sync solution(s) from java-practice

--- Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-0.java
+++ Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-0.java
@@ -5,20 +5,37 @@ import java.util.Arrays;
 public class SearchRange {
 
     public static int[] searchRange(int[] nums, int target) {
-        int[] result = new int[]{-1, -1};
-        if (nums.length == 0 || target < nums[0] || target > nums[nums.length - 1]) {
-            return result;
-        }
+        int[] result = {-1, -1};
+        int left = binarySearch(nums, target, true);
+        int right = binarySearch(nums, target, false);
+        result[0] = left;
+        result[1] = right;
+        return result;
+    }
+
+    private static int binarySearch(int[] nums, int target, boolean isSearchingLeft) {
         int left = 0;
         int right = nums.length - 1;
+        int idx = -1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                if ()
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                idx = mid;
+                if (isSearchingLeft) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             }
         }
 
-        return result;
+        return idx;
     }
 
     public static void main(String[] args) {
